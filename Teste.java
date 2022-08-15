03187f3830d52015809dc286a836fab9eb5e9315
pamella-silva <90103227+pampamella@users.noreply.github.com>
2022-08-14 23:49:32 -0300

    commit conexão com mysql

--- Teste.java
+++ Teste.java
@@ -1,5 +1,24 @@
+import src.config.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Teste {
         public static void main(String[] args) {
-            System.out.println("Hello world!");
+            Database MySql = new Database();
+            String url = MySql.url;
+            String username = MySql.username;
+            String password = MySql.password;
+
+            System.out.println(url);
+            System.out.println("Connecting database...");
+
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Database connected!");
+            } catch (SQLException e) {
+                throw new IllegalStateException("Cannot connect the database!", e);
+            }
         }
 }
---
+++ src/config/Database.java
@@ -0,0 +1,7 @@
+package src.config;
+
+public class Database {
+    public String url = "jdbc:mysql://localhost:3306/classicmodels";//System.getenv("MYSQL_JDBC_URL");
+    public String username = "java"; // System.getenv("MYSQL_USERNAME");
+    public String password = "senhamysql"; //System.getenv("MYSQL_PASSWORD");
+}
